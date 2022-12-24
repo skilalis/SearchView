cd1c08235eee7c76cf3816b4692cb9eada3a80b0
skilalis <skilalis@gmail.com>
2022-12-24 07:13:09 +0600

    Initial commit

---
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.searchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] countryNames;
+    private SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewId);
+        searchView = findViewById(R.id.searchViewId);
+
+        countryNames = getResources().getStringArray(R.array.country_name);
+
+        adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sample_view,R.id.textViewId,countryNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+}
